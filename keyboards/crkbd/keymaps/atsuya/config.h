3e5c54ccde4c353395537de7b24dc5c569825ac0
Atsuya Takagi <asoftonight@gmail.com>
2019-03-23 11:58:03 +0900

    update mouse setting

--- keyboards/crkbd/keymaps/atsuya/config.h
+++ keyboards/crkbd/keymaps/atsuya/config.h
@@ -44,10 +44,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_VAL_STEP 17
 
 // Mouse key
-#define MOUSEKEY_DELAY             30
-#define MOUSEKEY_INTERVAL          40
-#define MOUSEKEY_MAX_SPEED         6
-#define MOUSEKEY_TIME_TO_MAX       5
+#define MOUSEKEY_DELAY             0
+#define MOUSEKEY_INTERVAL          50
+#define MOUSEKEY_MAX_SPEED         5
+#define MOUSEKEY_TIME_TO_MAX       0
 #define MOUSEKEY_WHEEL_MAX_SPEED   8
 #define MOUSEKEY_WHEEL_TIME_TO_MAX 40
 
